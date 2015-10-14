26e0cce66832828ea1b7ba2cd57a88075a7198c8
marb <marb61a@gmail.com>
2015-10-14 10:19:01 +0000

    updated edit_posts.html and posts.js files

--- client/templates/admin/posts/posts.js
+++ client/templates/admin/posts/posts.js
@@ -16,3 +16,14 @@ Template.add_post.events({
         return false;
     }
 });
+
+Template.list_posts.events({
+	'click .delete_post': function(event){
+		if(confirm("Are you sure?")) {
+            Posts.remove(this._id);
+            FlashMessages.sendSuccess("Post Deleted");
+            // Prevent Submit
+            return false;
+        }
+	}
+});
