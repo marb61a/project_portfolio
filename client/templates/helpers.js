c2ebb92eed7c81b92308b4daad115c1b692e0dc5
marb <marb61a@gmail.com>
2015-10-19 13:44:57 +0000

    updated helpers.js and blog html

--- client/templates/helpers.js
+++ client/templates/helpers.js
@@ -22,5 +22,33 @@ Template.login.events({
 });
 
 Template.layout.events({
-    
+    'click .logout-user': function(event){
+		Meteor.logout(function(err){
+            if(err){
+                FlashMessages.sendError(err.reason);
+            } else {
+                FlashMessages.sendSuccess('You are now logged out');
+                Router.go('/');
+            }
+        });
+        
+        // Prevent Submit
+        return false;
+	}
+});
+
+Template.registerHelper('formatDate',function(date){
+    return moment(date).format('MMMM Do YYYY, h:mm:ss a');
+});
+
+Template.registerHelper('getSiteTitle', function(){
+	return 'CodeFolio';
+});
+
+Template.registerHelper('getAdminName', function(){
+	return 'John Doe';
+});
+
+Template.registerHelper('getAdminImage', function(){
+	return '/assets/img/user.png';
 });
