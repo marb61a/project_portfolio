514dbe75bd3a77e983f3c8ced3e373fff767480d
marb <marb61a@gmail.com>
2015-10-21 08:39:17 +0000

    updating router and troubleshooting migration issue

--- server/bootstrap.js
+++ server/bootstrap.js
@@ -1,9 +1,9 @@
 Meteor.startup(function(){
-    if(Meteor.users.find().count() === 0){
-        Accounts.createUser({
+	if(Meteor.users.find().count() === 0){
+		Accounts.createUser({
 			username: 'admin',
 			email: 'admin@admin.com',
 			password: 'password'
 		});
-    }
+	}
 });
