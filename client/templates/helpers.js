0b0517915ba0317c16a1561ed1c3cf0cb00a47e5
marb <marb61a@gmail.com>
2015-10-21 08:17:30 +0000

    updating roter and debugging

--- client/templates/helpers.js
+++ client/templates/helpers.js
@@ -1,10 +1,17 @@
+Template.work.helpers({
+	projects: function(){
+		return Projects.find();
+	}
+});
+
 Template.login.events({
-    'submit .login-user' : function(event){
-        var username = event.target.username.value;
-        var password = event.target.password.value;
-        
-        Meteor.loginWithPassword(username, password, function(err){
-            if(err){
+	'submit .login-user': function(event){
+		var username = event.target.username.value;
+		var password = event.target.password.value;
+
+
+		Meteor.loginWithPassword(username, password, function(err){
+			if(err){
 				event.target.username.value = username;
 				event.target.password.value = password;
 				FlashMessages.sendError(err.reason);
@@ -12,13 +19,13 @@ Template.login.events({
 				FlashMessages.sendSuccess('You are now logged in');
 				Router.go('/admin/projects');
 			}
-        });
-        
-        event.target.username.value = '';
+		});
+
+		event.target.username.value = '';
 		event.target.password.value = '';
 
 		return false;
-    }
+	}
 });
 
 Template.layout.events({
